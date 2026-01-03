f619fefebb6eaf7b389d1fdfc61d0a1a2f0064fa
Pkamal2002 <pkamal2002@gmail.com>
2026-01-03 18:23:33 +0530

    add SkillsForm component and integrate it into ResumeBuilder

--- src/pages/ResumeBuilder.jsx
+++ src/pages/ResumeBuilder.jsx
@@ -22,6 +22,7 @@ import ProfessionalSummaryForm from "../components/ProfessionalSummaryForm";
 import ExperienceForm from "../components/ExperienceForm";
 import EducationForm from "../components/EducationForm";
 import ProjectForm from "../components/ProjectForm";
+import SkillsForm from "../components/SkillsForm";
 
 const ResumeBuilder = () => {
   const { resumeId } = useParams();
@@ -166,6 +167,10 @@ const ResumeBuilder = () => {
                  {activeSection.id === "projects" && (
                   <ProjectForm data={resumeData.project}
                   onChange={(data)=>setResumeData(prev=>({...prev, project:data}))}/>
+                )}
+                {activeSection.id === "skills" && (
+                  <SkillsForm data={resumeData.skills}
+                  onChange={(data)=>setResumeData(prev=>({...prev, skills:data}))}/>
                 )}
 
                 {/* Future sections go here */}
